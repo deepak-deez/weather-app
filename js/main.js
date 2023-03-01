cc4a34e86390dbf3c84c1947f9450ef364ee9a17
Deepak Patra <deepak@itobuz.com>
2023-03-01 18:40:51 +0530

    first

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 const locate= document.querySelector('.location')
 
 const temp= document.querySelector('.temp')
+const expected= document.querySelector('.expected');
 
 
 
 
 
 
-async function getWeatherData() {
+
+async function getWeatherData(){
   const response = await fetch(
     "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=London&aqi=no"
   ).then(function (res) {
@@ -17,8 +19,8 @@ async function getWeatherData() {
 
 
   locate.innerHTML=response.location.name;
-  temp.innerHTML=response.current.feelslike_c;
-  
+  temp.innerHTML=response.current.temp_c+`<sup>o</sup>`;
+  expected.innerHTML=`feels like ${response.current.feelslike_c}<sup>o</sup> `;
 }
 
 getWeatherData();
