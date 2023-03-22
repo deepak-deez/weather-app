8c4b904b05d4fc7bf2154f65c16124608bcbe724
Deepak Patra <deepak@itobuz.com>
2023-03-22 12:31:00 +0530

    22 march

--- js/main.js
+++ js/main.js
@@ -2,41 +2,96 @@ const temp = document.querySelector(".temp");
 const expected = document.querySelector(".expected");
 const input = document.querySelector(".input-location");
 const inputDrop = document.querySelector(".input-dropdown");
-const citySelect = document.querySelectorAll(".input-dropdown li");
-
 const image = document.querySelector(".image-main");
 const imageSecondary = document.querySelector(".image-secondary");
-// let inputVal;
+
+(async() =>{
+    const response = await fetch(
+      "http://127.0.0.1:8081/all-cities") 
+      .then((data) => data.json())
+      .then((cityData)=>{
+        // console.log(cityData); 
+        let allCities=cityData;
+        console.log(allCities);
+        for(let i=0;i<allCities.length;i++){
+        let city = document.createElement('li');
+        city.innerHTML=allCities[i];
+        inputDrop.appendChild(city);
+        }
+      })
+      .catch((err) => {
+        let myCities = [
+          'kolkata',   'london',
+          'Ottawa',    'Mexico City',
+          'Brasilia',  'Paris',
+          'Chicago',   'Bali',
+          'Singapore', 'Oslo',
+          'Moscow',    'Kazan',
+          'Adana',     'Budapest',
+          'Barcelona', 'Manchester',
+          'Dubai',     'Surat',
+          'Panaji',    'Doha'
+        ]
+        for(let i=0;i<myCities.length;i++){
+          let city = document.createElement('li');
+          city.innerHTML=myCities[i];
+          inputDrop.appendChild(city);
+          }
+        console.log(err);
+      });
+
+      const citySelect = document.querySelectorAll(".input-dropdown li");
+      citySelect.forEach((ele) =>{
+
+        ele.addEventListener("click", ()=>{
+        input.value=ele.textContent;
+        // inputDrop.classList.toggle('hide');
+        getWeather();
+      });
+      })
+})()
+
 
 const getWeather = () => {
+  if(input.value.trim()==""){
+    alert("Enter a location!");
+    return;
+  }
   async function getWeatherData() {
     const response = await fetch(
-      "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=" +
-      input.value +
-        "&aqi=no"
+      "http://127.0.0.1:8081/" + input.value
+      // "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=" +
+      // input.value +
+      //   "&aqi=no"
     )
       .then((data) => data.json())
       .catch((err) => {
         console.log(err);
+        alert("Can't connect to Database. Check if server is listening.")
       });
     console.log("response", response);
-    temp.innerHTML = response.current.temp_c + `<sup>o</sup>`;
-    expected.innerHTML = `feels ${response.current.feelslike_c}<sup>o</sup> `;
+    // temp.innerHTML = response.current.temp_c + `<sup>o</sup>`;
+    // expected.innerHTML = `feels ${response.current.feelslike_c}<sup>o</sup> `;
+    temp.innerHTML = response.apiData.tempC + `<sup>o</sup>`;
+    expected.innerHTML = `feels ${response.apiData.condition.feelsLikeC}<sup>o</sup> `;
     setImg(response);
   }
   getWeatherData();
 };
 
 function setImg(response) {
-  let weatherValue = response.current.condition.text;
-  let dayValue = response.current.is_day;
+  // let weatherValue = response.current.condition.text;
+  let weatherValue = response.apiData.condition.text;
+  // let dayValue = response.current.is_day;
+  let dayValue = response.apiData.condition.isDay;
+  // let dayValue = 1;
+
   console.log(weatherValue);
   if (dayValue == 1) {
     image.setAttribute("src", "./images/sun.png");
   } else {
     image.setAttribute("src", "./images/moon.png");
   }
-
   if (weatherValue == "Overcast") {
     imageSecondary.setAttribute("src", "./images/overcast.png");
   } else if (weatherValue == "Sunny") {
@@ -56,6 +111,8 @@ function setImg(response) {
   }
 }
 
+// Light snow showers, Light rain shower
+
 input.addEventListener("keyup", (e) => {
   if (e.key == "Enter") {
     getWeather();
@@ -63,31 +120,26 @@ input.addEventListener("keyup", (e) => {
   }
 });
 
-
 input.onclick = ()=>{
   inputDrop.classList.toggle('hide')
 }
 
-citySelect.forEach((ele) =>{
-
-  ele.addEventListener("click", ()=>{
-  input.value=ele.textContent;
-  // inputDrop.classList.toggle('hide');
-  getWeather();
-});
-})
-
 window.addEventListener('click',(e)=>{
   if(e.target==input){
   inputDrop.classList.remove('hide');
-
   }
   else{
   inputDrop.classList.add('hide');
-
   }
 })
 
 // (()=>{
 //   getWeather()
 // })
+
+window.onload = runWeather(); 
+
+function runWeather(){  //weather of a default location
+input.value="London";
+getWeather();
+}
